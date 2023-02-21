4b9c9c995cf03c46f4e8136ad0b65fdf4605ab93
petrostrak <pit.trak@gmail.com>
2023-02-21 14:28:53 +0200

    Add test project

---
+++ my_workspace/host/000TestProject/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 21, 2023
+ *      Author: petros
+ */
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Hello world\n");
+
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+}
