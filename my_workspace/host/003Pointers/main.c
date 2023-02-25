6aa8484df9eb084ab8471dbff6179357b332efa5
petrostrak <pit.trak@gmail.com>
2023-02-25 12:17:29 +0200

    typecast righthand side explicitly

--- my_workspace/host/003Pointers/main.c
+++ my_workspace/host/003Pointers/main.c
@@ -5,7 +5,7 @@ int main()
     char x = 100;
     printf("The address of x is %p\n", &x);
 
-    char* ptr_x = &x; // create a pointer var and store the address of x
+    char* ptr_x = (char*)&x; // create a pointer var and store the address of x
     char value_of_x = *ptr_x;
     printf("The value of ptr_x is %d and the actual size of it is %d\n",value_of_x, sizeof(value_of_x));
 
