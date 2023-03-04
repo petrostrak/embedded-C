f7ef185580a089a2f35b2b8bab8b886bc3f07f05
petrostrak <pit.trak@gmail.com>
2023-03-04 20:33:15 +0200

    read input pin to change flow of leds

--- my_workspace/host/014TurnThemOn/Src/main.c
+++ my_workspace/host/014TurnThemOn/Src/main.c
@@ -6,31 +6,61 @@ void ledOFF(uint32_t*, uint8_t);
 
 int main(void)
 {
-	uint32_t *const pRCC = (uint32_t*) 0x40021014;
+	uint32_t *const pRCCE = (uint32_t*) 0x40021014;
+	uint32_t *const pRCCA = (uint32_t*) 0x40021014;
 	uint32_t *const pGPIOEmode = (uint32_t*) 0x48001000;
 	uint32_t *const pGPIOEoutput = (uint32_t*) 0x48001014;
+	uint32_t *const pGPIOAmode = (uint32_t*) 0x48000000;
+	uint32_t *const pGPIOAinput = (uint32_t*) 0x48000010;
 
 	// Enable the clock for GPIOE peripheral in the AHBENR
-	*pRCC |= (1 << 21); // SET the 21st bit position
+	*pRCCE |= (1 << 21); // SET the 21st bit position
+
+	// Enable the clock for GPIOA peripheral in the AHBENR
+	*pRCCA |= (1 << 17); // SET the 21st bit position
 
 	// Configure the mode of the IO pins as output.
 	*pGPIOEmode |= 0x55550000; // set the 8th-15th pin mode to 01 general purpose output mode)
 
+	// Configure the mode of the IO pin PA0 as input
+	*pGPIOAmode &= ~(3 << 0);
+
+
 	while(1)
 	{
-		for(uint8_t current = 8; current < 16; current++)
+		// Read the PA0 status (GPIOA input data register)
+		uint8_t PA0Status = (uint8_t)*pGPIOAinput & 0x1; // Since we only need to read the 1st bit, we zeroed all others
+
+		if(PA0Status)
 		{
-			uint8_t next = (current + 1) % 8;
+			*pGPIOEoutput &= 0x0; // Clear all output bits before start the iteration
+			for(uint8_t current = 8; current < 16; current++)
+			{
+				uint8_t next = (current + 1) % 8;
 
-			ledON(pGPIOEoutput, next);
+				ledON(pGPIOEoutput, next);
 
-			delay(1);
+				delay(1);
 
-			ledOFF(pGPIOEoutput, current);
+				ledOFF(pGPIOEoutput, current);
 
-			delay(1);
+				delay(1);
+			}
+		} else {
+			*pGPIOEoutput &= 0x0; // Clear all output bits before start the iteration
+			for(uint8_t current = 15; current >= 8; current--)
+			{
+				uint8_t next = (current - 1) % 8;
+
+				ledON(pGPIOEoutput, next);
+
+				delay(1);
+
+				ledOFF(pGPIOEoutput, current);
+
+				delay(1);
+			}
 		}
-
 	}
 
 }
