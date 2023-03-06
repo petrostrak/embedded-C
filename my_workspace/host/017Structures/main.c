59913a2c55c20d21264ba2c45c489bf691328615
petrostrak <pit.trak@gmail.com>
2023-03-06 16:18:45 +0200

    aligned-unaligned data access

--- my_workspace/host/017Structures/main.c
+++ my_workspace/host/017Structures/main.c
@@ -1,22 +1,68 @@
 /*
  * main.c
  *
- *  Created on: Mar 5, 2023
- *      Author: petrostrak
+ *  Created on: Mar 6, 2023
+ *      Author: petros
  */
 #include <stdio.h>
 #include <stdint.h>
-#include "car.h"
+
+struct DataSet
+{
+	char data1;
+	int data2;
+	char data3;
+	short data4;
+}__attribute__((packed));
+
+struct DataSetUnaligned
+{
+	char data1;
+	int data2;
+	char data3;
+	short data4;
+}__attribute__((packed));
 
 int main(void) {
-	struct CarModel BMW = {2021, 15000,220, 1330};
-	struct CarModel Honda = {
-			.carNumber = 2022,
-			.carPrice = 17600,
-			.carMaxSpeed = 210,
-			.carWeight = 15000,
-	};
+	struct DataSet data;
+	struct DataSetUnaligned data1;
 
-	printCar(BMW);
-	printCar(Honda);
+	data.data1 = 0x11;
+	data.data2 = 0xEEEEFFFF;
+	data.data3 = 0x22;
+	data.data4 = 0xABCD;
+
+	data1.data1 = 0x11;
+	data1.data2 = 0xEEEEFFFF;
+	data1.data3 = 0x22;
+	data1.data4 = 0xABCD;
+
+	uint8_t *ptr = (uint8_t*)&data;
+	uint32_t totalSize = sizeof(data);
+
+	printf("Memory address		Content\n");
+	printf("===========================\n");
+
+	for(uint32_t i = 0; i < totalSize; i++)
+	{
+		printf("%p		%X\n", ptr, *ptr);
+		ptr++;
+	}
+	printf("Total memory consumed by this struct variable is %lubytes, but should be 8bytes\n", sizeof(data));
+
+	uint8_t *ptr1 = (uint8_t*)&data1;
+	uint32_t totalSize1 = sizeof(data1);
+
+	printf("Memory address		Content\n");
+	printf("===========================\n");
+
+	for(uint32_t i = 0; i < totalSize1; i++)
+	{
+		printf("%p		%X\n", ptr1, *ptr1);
+		ptr1++;
+	}
+	printf("Total memory consumed by this struct variable is %lubytes, but should be 8bytes\n", sizeof(data1));
 }
+
+
+
