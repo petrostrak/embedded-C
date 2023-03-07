cdf3e203b81b0e8c49abd028efd1f67792bcc932
petrostrak <pit.trak@gmail.com>
2023-03-07 10:12:06 +0200

    Bit extraction

--- my_workspace/host/019PacketStruct/main.c
+++ my_workspace/host/019PacketStruct/main.c
@@ -26,5 +26,39 @@ int main(void)
 	int32_t value;
 	scanf("%X", &value);
 
+	Packet_t packet;
 
+	// bit extraction for crc
+	packet.crc = (uint8_t)(value & 0x3);
+
+	// bit extraction for status
+	packet.status = (uint8_t)(value >> 2) & 0x1;
+
+	// bit extraction for payload
+	packet.payload = (uint16_t) (value >> 3) & 0xFFF;
+
+	// bit extraction for bat
+	packet.bat = (uint8_t) (value >> 15) & 0x7;
+
+	// bit extraction for sensor
+	packet.sensor = (uint8_t) (value >> 18) & 0x7;
+
+	// bit extraction for longAddr
+	packet.longAddr = (uint8_t) (value >> 21) & 0xFF;
+
+	// bit extraction for shortAddr
+	packet.shortAddr = (uint8_t) (value >> 29) & 0x3;
+
+	// bit extraction for addrMode
+	packet.addrMode = (uint8_t) (value >> 31) & 0x1;
+
+	printf("crc:\t\t%#x\n", packet.crc);
+	printf("status:\t\t%#x\n", packet.status);
+	printf("payload:\t%#x\n", packet.payload);
+	printf("bat:\t\t%#x\n", packet.bat);
+	printf("sensor:\t\t%#x\n", packet.sensor);
+	printf("longAddr:\t%#x\n", packet.longAddr);
+	printf("shortAddr:\t%#x\n", packet.shortAddr);
+	printf("addrMode:\t%#x\n", packet.addrMode);
+	printf("Size of struct: %lu", sizeof(packet));
 }
